7df883c4ad74af10788946cdb8eed02608c78242
Joaquin Palacios <68148163+JoaquinPalacios@users.noreply.github.com>
2021-08-07 06:05:39 +1000

    nav expanded

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -6,8 +6,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const Navbar = () => {
+
+    const [expanded, setExpanded] = useState(false);
+
     return ( 
-        <NavbarBootstrap className='my-0' collapseOnSelect expand="lg">
+        <NavbarBootstrap className='my-0' collapseOnSelect expand="lg" expanded={expanded}>
             <div className='col-2 col-lg-1 logo ms-sm-3 me-0 pe-0'>
                 <Image alt='logo' src='/images/logo.png' width='75px' height='75px' />
             </div>
@@ -17,7 +20,7 @@ const Navbar = () => {
                     <a href='tel:+542235894137' alt='telefono' className='white'>2235-894137</a>
                 </span>
             </div> 
-            <NavbarBootstrap.Toggle aria-controls="responsive-navbar-nav" className="me-3" />
+            <NavbarBootstrap.Toggle aria-controls="responsive-navbar-nav" className="me-3" onClick={() => setExpanded(expanded ? false : "expanded")} />
             <NavbarBootstrap.Collapse className='col-4 col-lg-11 toggleBg' id="responsive-navbar-nav">
                 <Nav className='col-4 d-flex d-none d-xl-block fs-5 fst-italic justify-content-start align-self-end ms-0 ps-0'>
                     <FaQuoteLeft size='1.2em' className='pb-3' />Mudanzas en Mar del Plata y alrededores<FaQuoteRight size='1.2em' className='pb-3' />
@@ -28,9 +31,9 @@ const Navbar = () => {
                     </span>
                 </div>           
                 <Nav.Item className='col-8 col-lg-6 col-xl-4 d-flex justify-content-end align-self-center me-2'>
-                    <Link href="/"><a className='mx-3 my-2 pt-3 pt-md-0 my-lg-0' eventKey={1}>Inicio</a></Link>
-                    <Link href="/about"><a className='mx-3 my-2 my-lg-0' eventKey={2}>Quienes Somos</a></Link>
-                    <Link href="/contacto"><a className='ms-3 me-xl-2 my-2 my-lg-0' eventKey={3}>Contacto</a></Link>
+                    <Link href="/" onClick={() => setExpanded(false)}><a className='mx-3 my-2 pt-3 pt-md-0 my-lg-0'>Inicio</a></Link>
+                    <Link href="/about" onClick={() => setExpanded(false)}><a className='mx-3 my-2 my-lg-0'>Quienes Somos</a></Link>
+                    <Link href="/contacto" onClick={() => setExpanded(false)}><a className='ms-3 me-xl-2 my-2 my-lg-0'>Contacto</a></Link>
                 </Nav.Item>
             </NavbarBootstrap.Collapse>              
         </NavbarBootstrap>
