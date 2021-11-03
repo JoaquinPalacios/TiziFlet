ea4c9ab4a38f0e45979da2d59abbd03a8701f656
Joaquin Palacios <68148163+JoaquinPalacios@users.noreply.github.com>
2021-11-03 10:54:44 +1000

    og tags

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -8,6 +8,9 @@ const Layout = ({ children }) => {
             <Head>
               <meta name='viewport' content='initial-scale=1.0, width=device-width' />
               <link rel="icon" type="image/png" sizes="16x16" href="/images/logo.png" />
+              <meta property="og:title" content='Fletes TiziFlet' />
+              <meta property="og:type" content="website" />
+              <meta property="og:image" content='/images/og.png' />
             </Head>        
             <div>
                 <Navbar />
