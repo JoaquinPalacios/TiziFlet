dd546628796c72cdb99eea987f4d9aad14e6b906
Joaquin Palacios <68148163+JoaquinPalacios@users.noreply.github.com>
2021-11-07 11:58:55 +1000

    og image url

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -11,7 +11,7 @@ const Layout = ({ children }) => {
               <meta property="og:title" content='Fletes TiziFlet' />
               <meta property="og:description" content='Fletes y mudanzas en Mar del Plata y alrededores' />
               <meta property="og:type" content="website" />
-              <meta property="og:image" content="/images/mision.png" />
+              <meta property="og:image" content="https://www.tiziflet.com.ar/_next/image?url=%2F_next%2Fstatic%2Fimage%2Fpublic%2Fimages%2Fmision.fc4e03fc331008783b8be5d256c3fe76.jpg&w=3840&q=75" />
               <meta property="og:url" content="www.tiziflet.com.ar" />
             </Head>        
             <div>
