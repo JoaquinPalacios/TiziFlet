d15e8baeb770fe72fc168bbe4494c0ebb3495a8b
Joaquin Palacios <68148163+JoaquinPalacios@users.noreply.github.com>
2021-11-07 11:49:56 +1000

    og tags updated

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -9,8 +9,10 @@ const Layout = ({ children }) => {
               <meta name='viewport' content='initial-scale=1.0, width=device-width' />
               <link rel="icon" type="image/png" sizes="16x16" href="/images/logo.png" />
               <meta property="og:title" content='Fletes TiziFlet' />
+              <meta property="og:description" content='Fletes y mudanzas en Mar del Plata y alrededores' />
               <meta property="og:type" content="website" />
-              <meta property="og:image" content='/images/og.png' />
+              <meta property="og:image" content='/images/mision.png' />
+              <meta property="og:url" content="www.tiziflet.com.ar" />
             </Head>        
             <div>
                 <Navbar />
